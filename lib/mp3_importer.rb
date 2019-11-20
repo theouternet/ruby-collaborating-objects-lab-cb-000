a0ebda4728668eddfce942d4932bea0c37e62322
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-20 04:07:07 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -16,7 +16,12 @@ end
   
   def import
     self.files.each do |file|
-      file.split(" - ")
+      array = file.split(" - ")
+      artist = array[0].to_s 
+      song = array[1].to_s 
+      newartist = Artist.new(artist)
+      newsong = Song.new(song)
+      newsong.artist = newartist
   end
   
   
