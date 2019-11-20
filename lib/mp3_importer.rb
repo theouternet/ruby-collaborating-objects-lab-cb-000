6dc8b2bec6ebaf37e2f81857d30648b805ca9e4f
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-11-20 03:59:24 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,13 +1,13 @@
 class MP3Importer
   attr_accessor :path
   
-  @@files = []
-  
+
   def initialize(path)
     @path = path
   end
   
   def files 
+    files = []
     Dir.new(self.path).each do |file|
       files << file if file.length > 4
   end
